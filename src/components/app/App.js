b173479112a613c4f487eb82510bd7bd01cb8778
asosunoff <al.sosunoff@gmail.com>
2020-12-16 22:49:08 +0300

    first commit

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Modal from "../modal";
+import BaseButton from "../UI/button/baseButton";
+import useShow from "../../hooks/useShow";
+
+const App = () => {
+	const { isShow, show, hide } = useShow(false);
+
+	return (
+		<>
+			<Modal isShow={isShow} onHideModal={hide}>
+				<Modal.Title>Title</Modal.Title>
+
+				<Modal.Body>Body</Modal.Body>
+
+				<Modal.Footer>Footer</Modal.Footer>
+			</Modal>
+
+			<BaseButton onClick={isShow ? hide : show}>Показать</BaseButton>
+		</>
+	);
+};
+
+export default App;
